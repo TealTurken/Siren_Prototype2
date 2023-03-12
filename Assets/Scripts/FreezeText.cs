8cc0efbcd572e2dfd99fc80165ecf52ea087eac7
TealTurken <67345081+TealTurken@users.noreply.github.com>
2023-03-12 14:34:57 -0400

    Actual main menu text fix
    David Perez
    
    Complete fix of the main menu text

---
+++ Assets/Scripts/FreezeText.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeText : MonoBehaviour
+{
+    public float lockPos;
+
+    void Update()
+    {
+        // Locks the rotation.
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.z, lockPos, lockPos);
+    }
+}
